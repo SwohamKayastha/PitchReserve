9e00fbe9145c7e3215f1119fd778f08bb958253f
Swoham <swohamadditional@gmail.com>
2025-01-22 09:57:26 +0545

    added function to get futsalfield of each id

--- frontend/src/api/facilities.js
+++ frontend/src/api/facilities.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const API_URL = 'http://localhost:8000/futsal-facilities';
+const SCHEDULE_URL = 'http://localhost:8000/schedules';
 
 export const createOrUpdateFacility = async (formData) => {
   const token = localStorage.getItem('access_token');
@@ -11,6 +12,9 @@ export const createOrUpdateFacility = async (formData) => {
 
   try {
     const response = await axios.post(`${API_URL}/list/`, formData, { headers });
+    // Triggering the time slot generation for the new facility that is created from the form
+    const date = new Date().toISOString().split('T')[0];
+    await axios.post(`${SCHEDULE_URL}/generate-time-slots/${response.data.id}/${date}/`, {}, { headers });
     return response.data;
   } catch (error) {
     console.error('Error creating/updating facility:', error);
@@ -29,6 +33,47 @@ export const getFutsalFields = async () => {
     return response.data;
   } catch (error) {
     console.error('Error fetching futsal fields:', error);
+    throw error;
+  }
+};
+
+export const getFutsalFieldById = async (id) => {
+  const token = localStorage.getItem('access_token');
+  const headers = {
+    Authorization: `Bearer ${token}`,
+  };
+
+  try {
+    const response = await axios.get(`${API_URL}/detail/${id}/`, { headers });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching futsal field:', error);
+    throw error;
+  }
+};
+
+export const getOwnerFutsalFields = async () => {
+  const token = localStorage.getItem("access_token");
+  const headers = { Authorization: `Bearer ${token}` };
+
+  try {
+    const response = await axios.get(`${API_URL}/owner/facilities/`, { headers });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching owner fields:", error);
+    throw error;
+  }
+};
+
+export const getOwnerFutsalFieldById = async (id) => {
+  const token = localStorage.getItem("access_token");
+  const headers = { Authorization: `Bearer ${token}` };
+
+  try {
+    const response = await axios.get(`${API_URL}/owner/facilities/${id}/`, { headers });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching field by ID:", error);
     throw error;
   }
 };
